aea42dca3dc84e425694d088d027e498656b8adc
msftwarelab <msftwarelab@gmail.com>
2023-11-25 03:55:03 +0100

    getting meeting information

--- main.go
+++ main.go
@@ -1,10 +1,7 @@
 package main
 
 import (
-	"bufio"
-	"context"
 	"encoding/csv"
-	"fmt"
 	"io"
 	"log"
 	"os"
@@ -17,7 +14,6 @@ import (
 	"github.com/emersion/go-imap/v2/imapclient"
 	_ "github.com/emersion/go-message/charset"
 	"github.com/emersion/go-message/mail"
-	openai "github.com/sashabaranov/go-openai"
 	"github.com/spf13/viper"
 )
 
@@ -31,7 +27,11 @@ type MessageEntry struct {
 	Profession string
 	Company string
 	Date    time.Time
+	MeetingInfo string
+	Cc []imap.Address
 	isSent bool
+	isMeeting bool
+	isVirtualMeeting bool
 }
 
 type DatabaseEntry struct {
@@ -42,7 +42,11 @@ type DatabaseEntry struct {
 	LastDate    time.Time
 	Profession    string
 	Company    string
-	// Date    time.Time
+	TotalMeetings int
+	Cc string
+	VirtualMeetings int
+	PhysicalMeetings int
+	Relationship    string
 }
 
 func viperEnvVariable(key string) string {
@@ -72,20 +76,23 @@ func main() {
 // func fetchEmailsHandler(_c *gin.Context) {
 func fetchEmailsHandler() {
 	imapserver := "outlook.office365.com:993"
-	var useremail string
-	var password string
-	scanner := bufio.NewScanner(os.Stdin)
+	// var useremail string
+	// var password string
+	// scanner := bufio.NewScanner(os.Stdin)
 
-	fmt.Print("Enter your email address: ")
-	scanner.Scan()
-	useremail = scanner.Text()
+	// fmt.Print("Enter your email address: ")
+	// scanner.Scan()
+	// useremail = scanner.Text()
 
-	fmt.Print("Enter your password: ")
-	scanner.Scan()
-	password = scanner.Text()
+	// fmt.Print("Enter your password: ")
+	// scanner.Scan()
+	// password = scanner.Text()
 
-	// useremail := "miratest123@outlook.com"
-	// password := "Mira!Test!123"
+	useremail := "miratest123@outlook.com"
+	password := "Mira!Test!123"
+
+	// useremail := "acetopcloud@outlook.com"
+	// password := "cloudeast3k"
 	
 	log.Println("Processing emails...")
 
@@ -106,12 +113,19 @@ func fetchEmailsHandler() {
 
 	var messageEntries []MessageEntry
 	var databaseEntries []DatabaseEntry
-	receivedMessageEntries := processEmails(c, "INBOX", false)
-	sentMessageEntries := processEmails(c, "Sent", true)
+	receivedMessageEntries := processEmails(c, "INBOX", false, "inbox.txt")
+	sentMessageEntries := processEmails(c, "Sent", true, "sent.txt")
 	messageEntries = append(messageEntries, receivedMessageEntries...)
 	messageEntries = append(messageEntries, sentMessageEntries...)
 
+	log.Println("Extracting out spam messages...")
+	// Extracting out spam messages..
+
 	for _, entry := range messageEntries {
+		Cc := ""
+		for i := range entry.Cc {
+			Cc += entry.Cc[i].Mailbox + "@" + entry.Cc[i].Host + "; "
+		}
 		if(entry.isSent) {
 			entry.From = useremail
 		}
@@ -124,7 +138,18 @@ func fetchEmailsHandler() {
 		if !emailExistsInDatabase(email, databaseEntries) {
 			sentEmails := 0
 			receivedEmails := 0
+			totalMeetings := 0
+			virtualMeetings := 0
+			physicalMeetings := 0
 			name := ""
+			if entry.isMeeting {
+				totalMeetings = 1
+				if entry.isVirtualMeeting {
+					virtualMeetings = 1
+				} else {
+					physicalMeetings = 1
+				}
+			}
 			if entry.isSent {
 				name = entry.ToName
 				sentEmails++
@@ -134,18 +159,31 @@ func fetchEmailsHandler() {
 			}
 
 			databaseEntry := DatabaseEntry{
-				Name:            name,
-				Email:           email,
-				SentEmails:	     sentEmails,
-				ReceivedEmails:	 receivedEmails,
-				Profession:		 entry.Profession,
-				Company:		 entry.Company,
-				LastDate:		 entry.Date,
+				Name:              name,
+				Email:             email,
+				SentEmails:	       sentEmails,
+				ReceivedEmails:	   receivedEmails,
+				LastDate:		   entry.Date,
+				TotalMeetings:     totalMeetings,
+				Cc:                Cc,
+				VirtualMeetings:   virtualMeetings,
+				PhysicalMeetings:  physicalMeetings,
 			}
 			databaseEntries = append(databaseEntries, databaseEntry)
 		} else {
 			for i := range databaseEntries {
 				if databaseEntries[i].Email == email {
+					for i := range entry.Cc {
+						databaseEntries[i].Cc += entry.Cc[i].Mailbox + "@" + entry.Cc[i].Host + "; "
+					}
+					if entry.isMeeting {
+						databaseEntries[i].TotalMeetings++
+						if entry.isVirtualMeeting {
+							databaseEntries[i].VirtualMeetings++
+						} else {
+							databaseEntries[i].PhysicalMeetings++
+						}
+					}
 					if entry.To == useremail {
 						databaseEntries[i].ReceivedEmails++
 						if databaseEntries[i].Name == "" {
@@ -165,6 +203,15 @@ func fetchEmailsHandler() {
 		}
 	}
 
+	log.Println("Extracting profesion and company...")
+
+	for i := range databaseEntries {
+		profession, company, relationship := extractInformation(databaseEntries[i].Name, databaseEntries[i].Email, databaseEntries[i].SentEmails, databaseEntries[i].ReceivedEmails)
+		databaseEntries[i].Profession = profession
+		databaseEntries[i].Company = company
+		databaseEntries[i].Relationship = relationship
+	}
+
 	if err := exportToCSV(databaseEntries); err != nil {
 		log.Fatal(err)
 	}
@@ -173,7 +220,7 @@ func fetchEmailsHandler() {
 	log.Println("Done!")
 }
 
-func processEmails(c *imapclient.Client, mailboxName string, isSent bool) []MessageEntry {
+func processEmails(c *imapclient.Client, mailboxName string, isSent bool, fileName string) []MessageEntry {
 	var messageEntries []MessageEntry
 	log.Println("Fetching " + mailboxName + " messages...")
 	mbox, err := c.Select(mailboxName, nil).Wait()
@@ -189,6 +236,13 @@ func processEmails(c *imapclient.Client, mailboxName string, isSent bool) []Mess
 		Envelope: true,
 		BodySection: []*imap.FetchItemBodySection{{}},
 	}
+
+	file, err := os.Create(fileName)
+	if err != nil {
+		log.Fatal("Error creating file: ", err)
+	}
+	defer file.Close()
+	
 	messages := c.Fetch(seqSet, fetchOptions)
 	for {
 		msg := messages.Next()
@@ -196,9 +250,13 @@ func processEmails(c *imapclient.Client, mailboxName string, isSent bool) []Mess
 			break
 		}
 		content := ""
+		isMeeting := false
+		isVirtualMeeting := false
 		date := time.Now()
 		from := make([]imap.Address, 0)
 		to := make([]imap.Address, 0)
+		// fix the next line error		
+		Cc := []imap.Address{}
 		for {
 			item := msg.Next()
 			if item == nil {
@@ -229,6 +287,11 @@ func processEmails(c *imapclient.Client, mailboxName string, isSent bool) []Mess
 				if err != nil {
 					log.Fatal(err)
 				}
+
+				Cc = item.Envelope.Cc
+				if err != nil {
+					log.Fatal(err)
+				}
 			case imapclient.FetchItemDataBodySection:
 				mr, err := mail.CreateReader(item.Literal)
 				if err != nil {
@@ -249,12 +312,22 @@ func processEmails(c *imapclient.Client, mailboxName string, isSent bool) []Mess
 						if err != nil {
 							log.Fatal(err)
 						}
+						
 						if contentType == "text/plain" {
 							content = string(b)
 						} else if contentType == "text/html" {
 							if content == "" {
 								content = extractContent(string(b))
-						}
+							}
+						} else if contentType == "text/calendar" {
+							aaa, err := file.WriteString(string(b) + "==================================================================> \n\n")
+							abc, err := extractMeetingDates(string(b))
+							log.Println("=============> abc: ", abc)
+							isMeeting = true
+							isVirtualMeeting = isVirtual(string(b))
+							if err != nil {
+								log.Fatal("Error writing to file:", err, aaa)
+							}
 						}
 					case *mail.AttachmentHeader:
 						filename, _ := h.Filename()
@@ -263,7 +336,8 @@ func processEmails(c *imapclient.Client, mailboxName string, isSent bool) []Mess
 				}
 			}
 		}
-		profession, company := extractInformation(content)
+
+		// log.Println("================> content: ", content)
 
 		entry := MessageEntry{
 			Date:    	 date,
@@ -271,9 +345,11 @@ func processEmails(c *imapclient.Client, mailboxName string, isSent bool) []Mess
 			FromName:    from[0].Name,
 			To:      	 to[0].Mailbox + "@" + to[0].Host,
 			ToName:      to[0].Name,
-			Profession:  profession,
-			Company:   company,
 			isSent:   	 isSent,
+			Cc:          Cc,	
+			// MeetingInfo: meetingInfo,
+			isMeeting:   isMeeting,
+			isVirtualMeeting:   isVirtualMeeting,
 		}
 		messageEntries = append(messageEntries, entry)
 	}
@@ -337,52 +413,119 @@ func emailExistsInDatabase(email string, entries []DatabaseEntry) bool {
     return false
 }
 
-func extractInformation(content string) (string, string) {
-	log.Println("==============> content: ", content)
-	apiKey := "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"
-	ctx := context.Background()
-	client := openai.NewClient(apiKey)
-	prompt := `A person by the name of [x] has an email of [y], in one word each, what company do they work at (spell it out fully) and what is their likely profession?
-			   You sent them [x] emails and they responded with [y] email. Categorize the relationship. 
-			   In your response, only respond with "engaged," "sometimes engaged" or "unresponsive" in your response`
-	inputText := prompt + content
-	professionRegex := regexp.MustCompile(`Profession:\s*([^\n]+)`)
-	companyRegex := regexp.MustCompile(`Company:\s*([^\n]+)`)
+func extractInformation(name string, email string, sentEmails int, receivedEmails int) (string, string, string) {
+	// apiKey := "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"
+	// ctx := context.Background()
+	// client := openai.NewClient(apiKey)
+	// prompt1 := "A person by the name of " + name + "has an email of " + email + ", in one word each, what company do they work at (spell it out fully) and what is their likely profession? Output format is Profession, Company"
+	// prompt2 := "You sent them " + strconv.Itoa(sentEmails) + "emails and they responded with " + strconv.Itoa(sentEmails) + "email. Categorize the relationship. In your response, only respond with 'engaged,'' 'sometimes engaged' or 'unresponsive' in your response"
 
-	response, err := client.CreateChatCompletion(ctx,
-		openai.ChatCompletionRequest{
-			Model: openai.GPT4,
-			Messages: []openai.ChatCompletionMessage{
-				{
-					Role:    openai.ChatMessageRoleUser,
-					Content: inputText,
-				},
-			},
-			MaxTokens: 150,
-		},
-	)
-	if err != nil {
-		log.Fatal("Error generating response from OpenAI:", err)
-	}
+	// response1, err := client.CreateChatCompletion(ctx,
+	// 	openai.ChatCompletionRequest{
+	// 		Model: openai.GPT4,
+	// 		Messages: []openai.ChatCompletionMessage{
+	// 			{
+	// 				Role:    openai.ChatMessageRoleUser,
+	// 				Content: prompt1,
+	// 			},
+	// 		},
+	// 		MaxTokens: 150,
+	// 	},
+	// )
+	// if err != nil {
+	// 	log.Fatal("Error generating response from OpenAI:", err)
+	// }
 	
-	extractedInfo := response.Choices[0].Message.Content
-	log.Println("===========> extractedInfo: ", extractedInfo)
+	// result := response1.Choices[0].Message.Content
+	// match := extractCompanyProfession(result)
+	// profession, company := match[1], match[2]
+	// response2, err := client.CreateChatCompletion(ctx,
+	// 	openai.ChatCompletionRequest{
+	// 		Model: openai.GPT4,
+	// 		Messages: []openai.ChatCompletionMessage{
+	// 			{
+	// 				Role:    openai.ChatMessageRoleUser,
+	// 				Content: prompt2,
+	// 			},
+	// 		},
+	// 		MaxTokens: 150,
+	// 	},
+	// )
+	// if err != nil {
+	// 	log.Fatal("Error generating response from OpenAI:", err)
+	// }
+	
+	// relationship := response2.Choices[0].Message.Content
 
-	professionMatches := professionRegex.FindStringSubmatch(extractedInfo)
-	companyMatches := companyRegex.FindStringSubmatch(extractedInfo)
-	var profession, company string
-
-	if len(professionMatches) > 1 {
-		profession = strings.TrimSpace(professionMatches[1])
-	}
-
-	if len(companyMatches) > 1 {
-		company = strings.TrimSpace(companyMatches[1])
-	}
-
-	return profession, company
+	// return profession, company, relationship
+	return "hello", "hi", "good"
 }
 
+func extractCompanyProfession(input string) []string {
+	pattern := regexp.MustCompile(`(.+?),\s*(.+)`)
+	match := pattern.FindStringSubmatch(input)
+	return match
+}
+
+func extractMeetingInfo(rawInput string) string  {
+	organizerPattern := `ORGANIZER;[^:]+:mailto:([^@]+@[^:]+)`
+	organizerRegrex := regexp.MustCompile(organizerPattern)
+
+	organizer := organizerRegrex.FindStringSubmatch(rawInput)[1]
+	log.Println("=====> organizer: ", organizer)
+
+	return organizer
+}
+
+func extractMeetingDates(input string) ([]time.Time, error) {
+	datePattern := `\bDTSTART;[^\n]*:(\d{8}T\d{6}|\d{8})\b`
+	re := regexp.MustCompile(datePattern)
+	matches := re.FindAllStringSubmatch(input, -1)
+	var meetingDates []time.Time
+	for _, match := range matches {
+		if len(match) > 1 {
+			dateStr := match[1]
+			date, err := parseDate(dateStr)
+			if err != nil {
+				return nil, err
+			}
+			meetingDates = append(meetingDates, date)
+		}
+	}
+
+	return meetingDates, nil
+}
+
+func parseDate(dateStr string) (time.Time, error) {
+	layout := "20060102T150405"
+	if len(dateStr) == 8 {
+		layout = "20060102"
+	}
+	date, err := time.Parse(layout, dateStr)
+	if err != nil {
+		return time.Time{}, err
+	}
+
+	return date, nil
+}
+
+func isVirtual(rawInput string) bool {
+	var locations []string
+
+	lines := strings.Split(rawInput, "\n")
+	for _, line := range lines {
+		pattern := `^LOCATION(\S.*)`
+		re := regexp.MustCompile(pattern)
+		match := re.FindStringSubmatch(line)
+		if len(match) == 2 {
+			location := strings.TrimSpace(match[1])
+			location = strings.Replace(location, ":", "", 1)
+			location = strings.Replace(location, ";", "", 1)
+			locations = append(locations, location)
+		}
+	}
+	return locations[0] == "" || strings.HasPrefix(locations[0], "http://") || strings.HasPrefix(locations[0], "https://") || strings.HasPrefix(locations[0], "LANGUAGE")
+}
 func exportToCSV(entries []DatabaseEntry) error {
 	file, err := os.Create("messages.csv")
 	if err != nil {
@@ -393,7 +536,7 @@ func exportToCSV(entries []DatabaseEntry) error {
 	writer := csv.NewWriter(file)
 	defer writer.Flush()
 
-	header := []string{"Name", "Email", "SentEmails", "ReceivedEmails", "Profession", "Company", "LastDate"}
+	header := []string{"Name", "Email", "SentEmails", "ReceivedEmails", "Profession", "Company", "Others mentioned", "Total Meetings", "Virtual Meetings", "Physical Meetings", "Relationship", "LastDate"}
 	if err := writer.Write(header); err != nil {
 		return err
 	}
@@ -406,6 +549,11 @@ func exportToCSV(entries []DatabaseEntry) error {
 			strconv.Itoa(entry.ReceivedEmails),
 			entry.Profession,
 			entry.Company,
+			entry.Cc,
+			strconv.Itoa(entry.TotalMeetings),
+			strconv.Itoa(entry.VirtualMeetings),
+			strconv.Itoa(entry.PhysicalMeetings),
+			entry.Relationship,
 			entry.LastDate.Format("2006-01-02 15:04:05"),
 		}
 		if err := writer.Write(row); err != nil {
